63fd9a801de7f0c4e4b0f7a745a4919b29d4e3ba
hoklin12 <153165021+hoklin12@users.noreply.github.com>
2025-03-01 15:43:20 +0700

    BLA-007 – Implement a (first draft of) Rides Screen

--- lib/screens/ride/ride_screen.dart
+++ lib/screens/ride/ride_screen.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
+import '../../dummy_data/dummy_data.dart';
+import '../../model/ride/ride.dart';
 
-class FullScreenRideModal extends StatelessWidget {
-  const FullScreenRideModal({super.key});
+class RidesScreen extends StatelessWidget {
+  const RidesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Using the fakeRides from dummy_data.dart
+    final List<Ride> rides = fakeRides;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Full-Screen Modal"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
-      body: Center(
-        //
+      body: ListView.builder(
+        itemCount: rides.length,
+        itemBuilder: (context, index) {
+          final ride = rides[index];
+          return Card(
+            margin: const EdgeInsets.all(5),
+            child: ListTile(
+              title: Text(
+                "Departure: ${ride.departureLocation.name}\n${ride.arrivalLocation.name}",
+              ),
+              subtitle: Text(
+                "Time: ${ride.departureDate.hour}:${ride.departureDate.minute} | Seats: ${ride.availableSeats} | Price: €${ride.pricePerSeat}",
+              ),
+              onTap: () {
+                // Handle tap event
+              },
+            ),
+          );
+        },
       ),
     );
   }
