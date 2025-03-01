4d04d1c0776b4e248581624ada875525e5884918
hoklin12 <153165021+hoklin12@users.noreply.github.com>
2025-03-01 11:51:34 +0700

    BLA-006 – Add a Tween animation to show the Location Picker

--- lib/utils/animations_util.dart
+++ lib/utils/animations_util.dart
@@ -7,8 +7,8 @@ class AnimationUtils {
   /// Slide given screen from bottom to top
   ///
   static Route<T> createBottomToTopRoute<T>(Widget screen) {
-    const begin = Offset(0.0, 0.0);         //  TODO
-    const end = Offset(0.0, 0.0);           //  TODO
+    const begin = Offset(0.0, 1.0); // Start from bottom
+    const end = Offset(0.0, 0.0);   // End at normal position
     return _createAnimatedRoute(screen, begin, end);
   }
 
@@ -30,3 +30,36 @@ class AnimationUtils {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+//
+// class AnimationUtils {
+//   static const int transitionSpeed = 500; //ms
+//
+//   ///
+//   /// Slide given screen from bottom to top
+//   ///
+//   static Route<T> createBottomToTopRoute<T>(Widget screen) {
+//     const begin = Offset(0.0, 1.0); // Start from bottom
+//     const end = Offset(0.0, 0.0);   // End at normal position
+//     return _createAnimatedRoute(screen, begin, end);
+//   }
+//
+//   static Route<T> _createAnimatedRoute<T>(
+//       Widget screen, Offset begin, Offset end) {
+//     return PageRouteBuilder<T>(
+//       transitionDuration:
+//       const Duration(milliseconds: transitionSpeed), // Animation speed
+//       pageBuilder: (context, animation, secondaryAnimation) => screen,
+//       transitionsBuilder: (context, animation, secondaryAnimation, child) {
+//         var tween = Tween(begin: begin, end: end)
+//             .chain(CurveTween(curve: Curves.easeInOut));
+//
+//         return SlideTransition(
+//           position: animation.drive(tween),
+//           child: child,
+//         );
+//       },
+//     );
+//   }
+// }
